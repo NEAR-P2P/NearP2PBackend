26acea73401c4482436262c884d7056f7aef7141
hrpalencia <hrpmicarelli@gmail.com>
2022-04-12 12:24:16 -0400

    actualizacion

--- src/lib.rs
+++ src/lib.rs
@@ -1164,9 +1164,10 @@ impl NearP2P {
                 self.merchant[index].orders_completed = self.merchant[index].orders_completed + 1;
                 self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
 
-                let fee_deducted = (self.orders_sell[i].operation_amount * YOCTO_NEAR) * (0.003 as u128 * 10_000u128);
+                let fee_deducted = (self.orders_buy[i].operation_amount * 0.06) * YOCTO_NEAR;
+                let operation_amount = self.orders_buy[i].operation_amount as u128 * YOCTO_NEAR;
                 
-                Promise::new(self.orders_sell[i].owner_id.to_string()).transfer((self.orders_sell[i].operation_amount * YOCTO_NEAR) - fee_deducted);
+                Promise::new(self.orders_sell[i].owner_id.to_string()).transfer(operation_amount - fee_deducted);
 
                 Promise::new(self.vault.clone()).transfer(fee_deducted);
                 
@@ -1218,9 +1219,10 @@ impl NearP2P {
                 self.merchant[index].orders_completed = self.merchant[index].orders_completed + 1;
                 self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
                 
-                let fee_deducted = (self.orders_buy[i].operation_amount * YOCTO_NEAR) * (0.003 as u128 * 10_000u128);
+                let fee_deducted = (self.orders_buy[i].operation_amount * 0.06) * YOCTO_NEAR;
+                let operation_amount = self.orders_buy[i].operation_amount as u128 * YOCTO_NEAR; 
 
-                Promise::new(self.orders_buy[i].signer_id.to_string()).transfer((self.orders_buy[i].operation_amount * YOCTO_NEAR) - fee_deducted);
+                Promise::new(self.orders_buy[i].signer_id.to_string()).transfer(operation_amount - fee_deducted);
                 
                 Promise::new(self.vault.clone()).transfer(fee_deducted);
 
