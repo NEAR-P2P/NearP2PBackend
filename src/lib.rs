0bb4e6c2a51988f95de78ffa2f23baf4b0fc031e
hrpalencia <hrpmicarelli@gmail.com>
2022-04-23 14:57:24 -0400

    actualizacion

--- src/lib.rs
+++ src/lib.rs
@@ -1009,8 +1009,6 @@ impl NearP2P {
         if offer_type == 1 {
             for i in 0..self.offers_sell.len() {
                 if self.offers_sell.get(i).unwrap().offer_id == offer_id {
-                    env::log(format!("remainig amount en float: {} - remainig amount en yocto as u128: {} - atached deposit as f64: {}", self.offers_sell[i].remaining_amount ,
-                    (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as u128, attached_deposit as f64).as_bytes());
                     if (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as f64 >= attached_deposit as f64 {
                         ////////////////////////////////////////////////////////////////////
                         /* colocar aqui el bloqueo de saldo del owner_id  cuando sea venta */
