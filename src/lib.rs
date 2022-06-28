05569057f17bde460f84bc6607984b168370dccc
hrpalencia <hrpmicarelli@gmail.com>
2022-06-28 15:24:26 -0400

    actualizacion

--- src/lib.rs
+++ src/lib.rs
@@ -1283,71 +1283,76 @@ impl NearP2P {
 
 
         if offer_type == 1 {
-            for i in 0..self.offers_sell.len() {
-                if self.offers_sell.get(i).unwrap().offer_id == offer_id {
-                    if self.offers_sell[i].owner_id == env::signer_account_id() {
-                        env::panic_str("you can not accept your own offer");
-                    }
-                    //if (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as f64 >= attached_deposit as f64 {
-                    if (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as f64 >= amount as f64 {
-                        ////////////////////////////////////////////////////////////////////
-                        /* colocar aqui el bloqueo de saldo del owner_id  cuando sea venta */
-                        ////////////////////////////////////////////////////////////////////
-                        //let remaining: f64 = self.offers_sell[i].remaining_amount  - (attached_deposit as f64 / YOCTO_NEAR as f64) as f64;
-                        let remaining: f64 = self.offers_sell[i].remaining_amount  - (amount / YOCTO_NEAR as f64) as f64;
-                        if remaining <= 0.0 {
-                            self.offers_sell[i].status = 2;
+            let contract = self.contract_list.get(&env::signer_account_id());
+            if(contract.is_some()) {
+                for i in 0..self.offers_sell.len() {
+                    if self.offers_sell.get(i).unwrap().offer_id == offer_id {
+                        if self.offers_sell[i].owner_id == env::signer_account_id() {
+                            env::panic_str("you can not accept your own offer");
                         }
-                        
-                        if self.offers_sell[i].max_limit > remaining {
-                            self.offers_sell[i].max_limit = remaining;
-                        }
-                        if self.offers_sell[i].min_limit > remaining {
-                            self.offers_sell[i].min_limit = 1.0;
-                        }
-                        
-                        let fee = (amount as f64 / YOCTO_NEAR as f64) as f64 * FEE_TRANSACTION;
-                        let fee_deducted = (amount as f64 / YOCTO_NEAR as f64) as f64 - fee;
-                        self.offers_sell[i].remaining_amount = remaining;
-                        self.order_sell_id += 1;
-                        let data = OrderObject {
-                            offer_id: offer_id,
-                            order_id: self.order_sell_id,
-                            owner_id: self.offers_sell[i].owner_id.clone(),
-                            signer_id: env::signer_account_id(),
-                            exchange_rate: rate.to_string(), // self.offers_sell[i].exchange_rate.to_string(),
-                            operation_amount: (amount as f64 / YOCTO_NEAR as f64) as f64,
-                            fee_deducted: fee_deducted,
-                            payment_method: payment_method,
-                            fiat_method: self.offers_sell[i].fiat_method,
-                            confirmation_owner_id: 0,
-                            confirmation_signer_id: 0,
-                            confirmation_current: 0,
-                            time: self.offers_sell[i].time,
-                            datetime: datetime,
-                            terms_conditions: self.offers_sell[i].terms_conditions.to_string(),
-                            status: 1,
-                        };
-                        self.orders_sell.push(data);
-                        //actualizar total ordenes owner_id
-                        let mut index = self.merchant.iter().position(|x| x.user_id == self.offers_sell[i].owner_id.clone()).expect("owner not merchant");
-                        self.merchant[index].total_orders = self.merchant[index].total_orders + 1;
-                        self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
-                        index = self.merchant.iter().position(|x| x.user_id == env::signer_account_id().clone()).expect("owner not merchant");
-                        self.merchant[index].total_orders = self.merchant[index].total_orders + 1;
-                        self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
-                         
+                        //if (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as f64 >= attached_deposit as f64 {
+                        if (self.offers_sell[i].remaining_amount * YOCTO_NEAR as f64) as f64 >= amount as f64 {
+                            ////////////////////////////////////////////////////////////////////
+                            /* colocar aqui el bloqueo de saldo del owner_id  cuando sea venta */
+                            ////////////////////////////////////////////////////////////////////
+                            //let remaining: f64 = self.offers_sell[i].remaining_amount  - (attached_deposit as f64 / YOCTO_NEAR as f64) as f64;
+                            let remaining: f64 = self.offers_sell[i].remaining_amount  - (amount / YOCTO_NEAR as f64) as f64;
+                            if remaining <= 0.0 {
+                                self.offers_sell[i].status = 2;
+                            }
                             
-                        env::log_str("Offer sell accepted");
-                        // let msg: String = format!("Offer sell accepted - remaining: {} - Attached: {} - Amount: {}", self.offers_buy[i].remaining_amount, attached_deposit, amount.0);
-                        let msg: String = "Offer sell accepted".to_string();
-                        return String::from(msg);
-                    } else {
-                        // let error: String = format!("the quantity is greater than the offer sell amount - Remaining: {} - Attached: {}", self.offers_buy[i].remaining_amount, attached_deposit);
-                        // nv::panic(error.as_ref());
-                        env::panic_str("the quantity is greater than the offer sell amount");
+                            if self.offers_sell[i].max_limit > remaining {
+                                self.offers_sell[i].max_limit = remaining;
+                            }
+                            if self.offers_sell[i].min_limit > remaining {
+                                self.offers_sell[i].min_limit = 1.0;
+                            }
+                            
+                            let fee = (amount as f64 / YOCTO_NEAR as f64) as f64 * FEE_TRANSACTION;
+                            let fee_deducted = (amount as f64 / YOCTO_NEAR as f64) as f64 - fee;
+                            self.offers_sell[i].remaining_amount = remaining;
+                            self.order_sell_id += 1;
+                            let data = OrderObject {
+                                offer_id: offer_id,
+                                order_id: self.order_sell_id,
+                                owner_id: self.offers_sell[i].owner_id.clone(),
+                                signer_id: env::signer_account_id(),
+                                exchange_rate: rate.to_string(), // self.offers_sell[i].exchange_rate.to_string(),
+                                operation_amount: (amount as f64 / YOCTO_NEAR as f64) as f64,
+                                fee_deducted: fee_deducted,
+                                payment_method: payment_method,
+                                fiat_method: self.offers_sell[i].fiat_method,
+                                confirmation_owner_id: 0,
+                                confirmation_signer_id: 0,
+                                confirmation_current: 0,
+                                time: self.offers_sell[i].time,
+                                datetime: datetime,
+                                terms_conditions: self.offers_sell[i].terms_conditions.to_string(),
+                                status: 1,
+                            };
+                            self.orders_sell.push(data);
+                            //actualizar total ordenes owner_id
+                            let mut index = self.merchant.iter().position(|x| x.user_id == self.offers_sell[i].owner_id.clone()).expect("owner not merchant");
+                            self.merchant[index].total_orders = self.merchant[index].total_orders + 1;
+                            self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
+                            index = self.merchant.iter().position(|x| x.user_id == env::signer_account_id().clone()).expect("owner not merchant");
+                            self.merchant[index].total_orders = self.merchant[index].total_orders + 1;
+                            self.merchant[index].percentaje_completion = (self.merchant[index].orders_completed as f64 / self.merchant[index].total_orders as f64) * 100.0;
+                            
+                                
+                            env::log_str("Offer sell accepted");
+                            // let msg: String = format!("Offer sell accepted - remaining: {} - Attached: {} - Amount: {}", self.offers_buy[i].remaining_amount, attached_deposit, amount.0);
+                            let msg: String = "Offer sell accepted".to_string();
+                            return String::from(msg);
+                        } else {
+                            // let error: String = format!("the quantity is greater than the offer sell amount - Remaining: {} - Attached: {}", self.offers_buy[i].remaining_amount, attached_deposit);
+                            // nv::panic(error.as_ref());
+                            env::panic_str("the quantity is greater than the offer sell amount");
+                        }
                     }
                 }
+            } else if(contract.is_none()) {
+                env::panic_str("no tiene un contrato desplegado parta aceptar la oferta de venta");
             }
             return String::from("Offer sell not found");
         } else if offer_type == 2 {
