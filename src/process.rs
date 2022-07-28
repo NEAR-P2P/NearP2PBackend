815bb53d0c06504de5c0b6309a4e300c97ed3cd4
hrpalencia <hrpmicarelli@gmail.com>
2022-07-28 18:52:12 -0400

    actualizar

--- src/process.rs
+++ src/process.rs
@@ -294,7 +294,7 @@ impl NearP2P {
                     data_contract.contract,
                     env::current_account_id(),
                     0,
-                    Gas(9_000_000_000_000),
+                    Gas(20_000_000_000_000),
                 ));
             } 
 
