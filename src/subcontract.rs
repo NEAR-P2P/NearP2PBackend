2f29ebba5b55c2876ab24909f56945abf98d1f19
hrpalencia <hrpmicarelli@gmail.com>
2022-08-15 11:16:34 -0400

    act

--- src/subcontract.rs
+++ src/subcontract.rs
@@ -223,7 +223,7 @@ impl NearP2P {
             contract.contract.clone(),
             env::current_account_id(),
             0,
-            Gas(90_000_000_000_000),
+            Gas(100_000_000_000_000),
         ));
     }
 
@@ -250,7 +250,7 @@ impl NearP2P {
             signer_id.clone(),
             env::current_account_id(),
             0,
-            Gas(80_000_000_000_000),
+            Gas(70_000_000_000_000),
         ));
     }
 
